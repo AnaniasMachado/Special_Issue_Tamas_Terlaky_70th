0fde3df854bd89e51870f368cb5cae31668a78c1
Ananias Machado <86498679+AnaniasMachado@users.noreply.github.com>
2025-09-02 17:37:41 -0400

    Including sliced plots for norm 0 and norm 1

---
+++ src/plots/run_plots_P123_2.jl
@@ -0,0 +1,36 @@
+using CSV
+using DataFrames
+using Plots
+using LaTeXStrings
+
+problem = "P123"
+time_begin = 50
+time_end = 1200
+start = 1
+step = 10
+
+admm_filepath = "results/problem_$(problem)/run_data_$(problem)_ADMM.csv"
+drs_opt_filepath = "results/problem_$(problem)/run_data_$(problem)_DRS_Opt.csv"
+drs_fp_filepath  = "results/problem_$(problem)/run_data_$(problem)_DRS_FP.csv"
+
+df_admm = CSV.read(admm_filepath, DataFrame)
+df_drs_opt = CSV.read(drs_opt_filepath, DataFrame)
+df_drs_fp  = CSV.read(drs_fp_filepath, DataFrame)
+
+df_admm = df_admm[(df_admm.time .>= time_begin) .& (df_admm.time .<= time_end), :]
+df_drs_opt = df_drs_opt[(df_drs_opt.time .>= time_begin) .& (df_drs_opt.time .<= time_end), :]
+df_drs_fp  = df_drs_fp[(df_drs_fp.time .>= time_begin) .& (df_drs_fp.time .<= time_end), :]
+
+df_admm = df_admm[start:step:end, :]
+df_drs_opt = df_drs_opt[start:step:end, :]
+df_drs_fp  = df_drs_fp[start:step:end, :]
+
+default(markersize=3)
+
+scatter(df_drs_fp.time, df_drs_fp.H_norm_0, label=L"\textrm{DRS}_{FP}", xlabel=L"\textrm{time}", ylabel=L"||\textrm{H}||_0")
+scatter!(df_admm.time, df_admm.H_norm_0, label=L"\textrm{ADMM}")
+savefig("Plots/problem_$(problem)/problem_$(problem)_H_norm_0_sliced.png")
+
+scatter(df_drs_fp.time, df_drs_fp.H_norm_1, label=L"\textrm{DRS}_{FP}", xlabel=L"\textrm{time}", ylabel=L"||\textrm{H}||_1")
+scatter!(df_admm.time, df_admm.H_norm_1, label=L"\textrm{ADMM}")
+savefig("Plots/problem_$(problem)/problem_$(problem)_H_norm_1_sliced.png")
